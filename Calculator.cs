5cd3a4ad0dc431af1ac8071925706ff5da6415a7
Adrian Widlak <adrian.widlak@codecool.com>
2024-01-21 13:51:59 +0100

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    public class Calculator
+    {
+        private readonly object _lock = new object();
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int AddInLoop(int x, int y)
+        {
+            int suma = x;
+            var (counter, increment) = y >= 0 ? (y,  1) : (-y, -1);
+
+            for(int i = 0; i < counter; i++)
+            {
+                suma += increment;
+            }
+            return suma;
+        }
+
+
+        public int AddInLoopWithThreads(int x, int y)
+        {
+            int suma = x;
+            var (counter, increment) = y >= 0 ? (y, 1) : (-y, -1);
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    int result = suma;
+                    Thread.Sleep(new Random().Next(1, 15));
+                    result += increment;
+                    suma = result;
+                });
+
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return suma;
+        }
+
+
+        public int AddInLoopWithThreadsLock(int x, int y)
+        {
+            int suma = x;
+            var (counter, increment) = y >= 0 ? (y, 1) : (-y, -1);
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    //int result = suma;
+                    Thread.Sleep(new Random().Next(1, 15));
+                    //result += increment;
+                    //suma = result;
+                    Interlocked.Add(ref suma, increment);
+                });
+
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return suma;
+        }
+
+
+        public int AddInLoopWithThreadsLockPrimitive(int x, int y)
+        {
+            int suma = x;
+            var (counter, increment) = y >= 0 ? (y, 1) : (-y, -1);
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    //int result = suma;
+                    Thread.Sleep(new Random().Next(1, 15));
+                    //result += increment;
+                    //suma = result;
+                    //Interlocked.Add(ref suma, increment);
+
+                    lock (_lock)
+                    {
+                        suma += increment;
+
+                    }
+                });
+
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return suma;
+        }
+
+
+        public int AddInLoopWithThreadsMutex(int x, int y)
+        {
+            int suma = x;
+            var (counter, increment) = y >= 0 ? (y, 1) : (-y, -1);
+            var threads = new List<Thread>();
+
+            using var mutex = new Mutex();
+
+            for (int i = 0; i < counter; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    Thread.Sleep(new Random().Next(1, 15));
+
+                    // Complicated operation
+
+                    mutex.WaitOne();
+                    suma += increment;
+                    mutex.ReleaseMutex();
+                });
+
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return suma;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            int sum = 0;
+            sum = calculator.Add(35, 15);
+            Console.WriteLine($"Add: {sum}");
+
+            sum = calculator.AddInLoop(35, 15);
+
+            Console.WriteLine($"AddInLoop: {sum}");
+
+            sum = calculator.AddInLoopWithThreads(35, 15);
+            Console.WriteLine($"AddInLoopWithThreads: {sum}");
+
+            sum = calculator.AddInLoopWithThreadsLock(35, 15);
+            Console.WriteLine($"AddInLoopWithThreads: {sum}");
+
+            sum = calculator.AddInLoopWithThreadsLockPrimitive(35, 15);
+            Console.WriteLine($"AddInLoopWithThreadsLockPrimitive: {sum}");
+
+            sum = calculator.AddInLoopWithThreadsMutex(35, 15);
+            Console.WriteLine($"AddInLoopWithThreadsMutex: {sum}");
+        }
+    }
+}
